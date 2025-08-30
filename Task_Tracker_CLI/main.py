02678c1c239388377b02a6db56a8b28e3bd41b2a
saad <jdiasaad86@gmail.com>
2025-08-30 17:19:41 +0100

    add the update choice and import the update function

--- Task_Tracker_CLI/main.py
+++ Task_Tracker_CLI/main.py
@@ -1,6 +1,7 @@
 from show_task import show_tasks
 from add_task import add_task
 from delete_task import delete_task
+from update_task import update_task
 import sys
 
 def show_menu():
@@ -8,7 +9,8 @@ def show_menu():
     print("1. View tasks")
     print("2. Add task")
     print("3. delete task")
-    print("4. Exit")
+    print("4. update task")
+    print("5. Exit")
        
 while True:
     task_file = "tasks.json"
@@ -22,10 +24,12 @@ while True:
         elif choice == 3:
             delete_task(task_file)
         elif choice == 4:
+            update_task(task_file)
+        elif choice == 5:
             print("Goodbye!")
             break
         else:
             print("Invalid choice. Try again.") 
-    except Exception:
+    except ValueError:
         print("invalid")
     
