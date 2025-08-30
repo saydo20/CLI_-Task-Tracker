5d01ff2ca32a34570ce2050edf44bd26cef9cb9f
saad <jdiasaad86@gmail.com>
2025-08-30 16:26:13 +0100

    import the delete function and store the file name in a variable

--- Task_Tracker_CLI/main.py
+++ Task_Tracker_CLI/main.py
@@ -1,22 +1,31 @@
 from show_task import show_tasks
 from add_task import add_task
+from delete_task import delete_task
 import sys
 
 def show_menu():
     print("\n--- Task Manager ---")
     print("1. View tasks")
     print("2. Add task")
-    print("3. Exit")
+    print("3. delete task")
+    print("4. Exit")
        
 while True:
+    task_file = "tasks.json"
     show_menu()
-    choice = int(input("What is your choice: "))
-    if choice == 1:
-        show_tasks()
-    elif choice == 2:
-        add_task()
-    elif choice == 3:
-        print("Goodbye!")
-        break
-    else:
-        print("Invalid choice. Try again.")
+    try:
+        choice = int(input("What is your choice: "))
+        if choice == 1:
+            show_tasks(task_file)
+        elif choice == 2:
+            add_task(task_file)
+        elif choice == 3:
+            delete_task(task_file)
+        elif choice == 4:
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Try again.") 
+    except Exception:
+        print("invalid")
+    
